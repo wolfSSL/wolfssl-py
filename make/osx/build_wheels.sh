2982c718a038ea448420136a0606564e9621e99e
John Safranek <john@wolfssl.com>
2019-09-27 15:25:37 -0700

    Build Update 1. Replaced the versioned venv paths in gitignore with a wildcard, like in the wolfCrypt project. 2. Remove the absolute path to the python interpreter. They just need to be in the path. (For example, you might install old versions in /opt and add their subdirs to the path while the latest runs out of its location.) 3. Build the name of python, pip, and venv_ based on the version.

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -1,30 +1,29 @@
 set -e
 set +x
 
-for PYVERSION in 2.7 3.4 3.5 3.6; do
-    PYTHONBIN=/Library/Frameworks/Python.framework/Versions/${PYVERSION}/bin
-    PYTHONEXE=${PYTHONBIN}/python${PYVERSION}
-    export PATH="${PYTHONBIN}":$PATH
+for VER in 2.7 3.4 3.5 3.6 3.7; do
+    PYTHON=python${VER}
+    PIP=pip${VER}
+    VENV=venv_${VER}
 
     # update pip for newer TLS support
-    curl https://bootstrap.pypa.io/get-pip.py | ${PYTHONEXE}
-    "${PYTHONBIN}/pip" install --upgrade pip
+    curl https://bootstrap.pypa.io/get-pip.py | ${PYTHON}
+    ${PIP} install --upgrade pip
 
     # update virtualenv
-    ${PYTHONEXE} -m pip install virtualenv
-
-    virtualenv -p ${PYTHONEXE} venv_${PYVERSION}
-    . ./venv_${PYVERSION}/bin/activate
+    ${PIP} install --upgrade virtualenv
+    virtualenv -p ${PYTHON} ${VENV}
+    . ./${VENV}/bin/activate
 
     # install cffi module
-    ${PYTHONEXE} -m pip install cffi
+    ${PYTHON} -m pip install cffi
 
-    ${PYTHONEXE} setup.py bdist_wheel
-    "${PYTHONBIN}/pip" install -r requirements/test.txt
+    ${PYTHON} setup.py bdist_wheel
+    ${PIP} install -r requirements/test.txt
     set +e
-    "${PYTHONBIN}/pip" uninstall -y wolfssl
+    ${PIP} uninstall -y wolfssl
     set -e
-    "${PYTHONBIN}/pip" install wolfssl --no-index -f dist
+    ${PIP} install wolfssl --no-index -f dist
     rm -rf tests/__pycache__
     py.test tests
     deactivate
