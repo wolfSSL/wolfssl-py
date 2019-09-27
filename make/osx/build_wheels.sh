fc88398f8b732f86cb126b3d64789ce2f6c7a0e7
John Safranek <john@wolfssl.com>
2019-09-27 16:09:52 -0700

    Build Update One more update to make the build_wheels scripts the same.

--- make/osx/build_wheels.sh
+++ make/osx/build_wheels.sh
@@ -2,21 +2,18 @@ set -e
 set +x
 
 for VER in 2.7 3.4 3.5 3.6 3.7; do
-    PYTHON=python${VER}
-    PIP=pip${VER}
-    VENV=venv_${VER}
+    PIP="pip${VER}"
+    PYTHON="python${VER}"
+    VENV="venv_${VER}"
 
     # update pip for newer TLS support
     curl https://bootstrap.pypa.io/get-pip.py | ${PYTHON}
-    ${PIP} install --upgrade pip
+    ${PIP} install -r requirements/setup.txt
 
     # update virtualenv
     ${PIP} install --upgrade virtualenv
     virtualenv -p ${PYTHON} ${VENV}
     . ./${VENV}/bin/activate
-
-    # install cffi module
-    ${PYTHON} -m pip install cffi
 
     ${PYTHON} setup.py bdist_wheel
     ${PIP} install -r requirements/test.txt
